990ca346def6b931b64c37a7a0c5c61a741be07b
Zilong <b0150107@ntu.edu.tw>
2017-03-26 19:07:13 +0800

    第一版

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+
+    public GameObject Camera;
+    public GameObject StonePrefab;
+    [Header("產生球的位置")]
+    public Transform InsPosition;
+    [Header("產生球時把彈弓的兩點給予球")]
+    public Transform LinePoint1;
+    public Transform LinePoint2;
+
+    public bool IsWin;
+
+
+    private GameObject _Stone;
+    //可以使用的石頭數量，超過還沒打到小鳥就輸了
+    private int _StoneNum = 3;
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+    }
+    void Init()
+    {
+        _StoneNum = 3;
+        InsNewStone();
+    }
+    public void InsNewStone()
+    {
+        _Stone = (GameObject)Instantiate(StonePrefab, InsPosition.position, Quaternion.identity);
+        _Stone.GetComponent<Stone>().m_GameMain = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int GetStoneNum()
+    {
+        return _StoneNum;
+    }
+}
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    [Header("彈弓上的兩個點")]
+    public Transform LinePoint1, LinePoint2;
+    public bool IsLine = true;
+    [Range(0, 1.5f)]
+    public float DragRadius;
+    public float ForcePower;
+
+    public GameMain m_GameMain;
+    Vector2 DragCenterPos;
+    Vector2 OffsetFromCenter;
+
+    bool isCanDrag;
+    Transform m_Transform;
+    LineRenderer LineRenderer1, LineRenderer2;
+    Rigidbody2D m_Rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+
+        LineRenderer1 = LinePoint1.GetComponent<LineRenderer>();
+        LineRenderer2 = LinePoint2.GetComponent<LineRenderer>();
+        LineRenderer1.sortingLayerName = "Line";
+        LineRenderer2.sortingLayerName = "Line";
+        DragCenterPos = (LinePoint1.position + LinePoint2.position) / 2;
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Transform = GetComponent<Transform>();
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsLine)
+        {
+            DrawLine();
+        }
+    }
+    public void Init()
+    {
+        isCanDrag = true;
+        m_Rigidbody2D.isKinematic = true;
+        IsLine = true;
+
+    }
+    void DrawLine()
+    {
+        LineRenderer1.SetPosition(0, LinePoint1.position);
+        LineRenderer1.SetPosition(1, m_Transform.position);
+        LineRenderer2.SetPosition(0, LinePoint2.position);
+        LineRenderer2.SetPosition(1, m_Transform.position);
+
+
+
+    }
+    public void Shoot(Vector2 force)
+    {
+        isCanDrag = false;
+        IsLine = false;
+        LineRenderer1.SetPosition(1, LinePoint1.position);
+        LineRenderer2.SetPosition(1, LinePoint2.position);
+        m_Rigidbody2D.isKinematic = false;
+        float power = ForcePower / DragRadius * force.magnitude;
+        print(force + "" + power);
+        m_Rigidbody2D.AddForce(force.normalized * power, ForceMode2D.Force);
+    }
+
+
+    private void OnMouseDrag()
+    {
+        if (isCanDrag)
+        {
+            Vector2 mouse = Input.mousePosition;
+            Vector2 newpos = Camera.main.ScreenToWorldPoint(mouse);
+            OffsetFromCenter = DragCenterPos - newpos;
+
+            if (OffsetFromCenter.magnitude > DragRadius)
+            {
+                Vector2 offsetz = DragCenterPos - new Vector2(m_Transform.position.x, m_Transform.position.y);
+                Shoot(offsetz);
+            }
+            else
+            {
+                transform.position = newpos;
+            }
+        }
+
+    }
+    private void OnMouseUp()
+    {
+        if (isCanDrag)
+        {
+            Vector2 offset = DragCenterPos - new Vector2(m_Transform.position.x, m_Transform.position.y);
+            Shoot(offset);
+        }
+    }
+    void DestroySelf()
+    {
+        if (m_GameMain.GetStoneNum() > 0)
+        {
+            m_GameMain.InsNewStone();
+        }
+        Destroy(this.gameObject);
+    }
+}
